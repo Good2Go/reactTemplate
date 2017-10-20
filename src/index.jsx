4d218422522d509bc51c912a59538f09e67e54d0
Damanpreet <daman.4880@gmail.com>
2017-10-20 22:34:16 +0530

    09 - Create Redux Store

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+
+// Redux
+
+import configureStore from './stores/index.jsx';
+
 import App from './components/app.jsx';
+
+const store = configureStore();
 
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
+  <App />,
+  document.getElementById('app'),
 );
